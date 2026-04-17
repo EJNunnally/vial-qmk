db3bac41ebb0dbaac6b4a6bca5a4762223c27fb9
erik <ejnunnally@gmail.com>
2026-04-17 00:35:15 -0600

    keymap for the wifey

--- keyboards/lily58/keymaps/sariahlbriggs/rules.mk
+++ keyboards/lily58/keymaps/sariahlbriggs/rules.mk
@@ -9,7 +9,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
+OLED_ENABLE = no            # OLED display
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 WPM_ENABLE = no
@@ -24,11 +24,3 @@ CONSOLE_ENABLE = no
 QMK_SETTINGS = no
 
 KEY_OVERRIDE_ENABLE = no
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
