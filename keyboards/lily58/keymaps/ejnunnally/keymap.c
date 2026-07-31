89e2807ae6439e1c2175268d1ecba7dcb24a8010
erik <ejnunnally@gmail.com>
2026-07-31 13:29:10 -0700

    modified lily58 keymap

--- keyboards/lily58/keymaps/ejnunnally/keymap.c
+++ keyboards/lily58/keymaps/ejnunnally/keymap.c
@@ -1,10 +1,8 @@
 #include QMK_KEYBOARD_H
 
-// to change keymap, use
-// make lily58:(any folder):avrdude
-
 enum layer_number {
     BSE = 0,
+    // QWE, // excluded because it makes the firmware too large, I think -- Erik 2026-07-31
     LWR,
     RSE
 };
@@ -19,15 +17,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LALT, KC_LGUI, MO(LWR),  KC_SPC,  KC_ENT, KC_BSPC,  KC_DEL,  MO(RSE)
             ),
 
-    [LWR] = LAYOUT( // I need the 3 key for Minecraft's F3 + 3 hotkey
-             KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
+    // [QWE] = LAYOUT(
+    //         KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+    //         KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+    //         KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    //         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MINS,  KC_EQL,    KC_N,    KC_M,  KC_DOT, KC_COMM, KC_SLSH, KC_RSFT,
+    //         KC_LALT, KC_LGUI, MO(LWR),  KC_SPC,  KC_ENT, KC_BSPC,  KC_DEL,  MO(RSE)
+    //         ),
+
+    [LWR] = LAYOUT(
+            KC_F11,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
             _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, KC_CAPS,
             _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
             _______, _______, _______, _______, _______, _______, _______, _______
             ),
 
-    [RSE] = LAYOUT( // stupid Windows and their ALT codes
+    [RSE] = LAYOUT(
             _______, XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                    KC_INS, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, XXXXXXX,   MS_UP, XXXXXXX, MS_ACL0,                    KC_NUM,   KC_P7,   KC_P8,   KC_P9, XXXXXXX, XXXXXXX,
             _______, MS_BTN2, MS_LEFT, MS_DOWN, MS_RGHT, MS_ACL1,                   XXXXXXX,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, KC_CAPS,
@@ -53,12 +59,18 @@ bool oled_task_user(void) {
             case BSE:
                 oled_write_P(PSTR("     \n"), false);
                 break;
+            // case QWE:
+            //     oled_write_P(PSTR("QWERT\n"), false);
+            //     break;
             case LWR:
                 oled_write_P(PSTR("LOWER\n"), false);
                 break;
             case RSE:
                 oled_write_P(PSTR("RAISE\n"), false);
                 break;
+            // case LAY:
+            //     oled_write_P(PSTR("LAYER\n"), false);
+            //     break;
             default:
                 // Or use the write_ln shortcut over adding '\n' to the end of your string
                 oled_write_ln_P(PSTR("UNDEF\n"), false);
